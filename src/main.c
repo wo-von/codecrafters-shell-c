2aad27ae840cf5a510c074415b2bf8ed09ea81e4
sina hosseinizad <ssh@ammonit.com>
2025-12-12 16:43:36 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -11,6 +11,8 @@ int main(int argc, char *argv[]) {
   while (1) {
     fgets(command, sizeof(command), stdin);
     command[strcspn(command, "\n")] = '\0'; 
+    if (strcmp(command, "exit") == 0) break;
+
     printf("%s: command not found\n$ ", command);
   }
   return 0;

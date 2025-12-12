05873fb7015578a11e4a52df309a3ba8f557146b
sina hosseinizad <ssh@ammonit.com>
2025-12-12 15:30:13 +0000

    stage 3

--- src/main.c
+++ src/main.c
@@ -6,10 +6,12 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   char command[1024];
-  
+
   printf("$ ");
-  fgets(command, sizeof(command), stdin);
-  command[strcspn(command, "\n")] = '\0'; 
-  printf("%s: command not found\n", command);
+  while (1) {
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = '\0'; 
+    printf("%s: command not found\n$ ", command);
+  }
   return 0;
 }

32c05de3308089eae551df17f9f84808e6841637
sina hosseinizad <ssh@ammonit.com>
2025-12-12 13:34:13 +0100

    testing stage 2

--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-
-  // TODO: Uncomment the code below to pass the first stage
+  char command[1024];
+  
   printf("$ ");
-
+  fgets(command, sizeof(command), stdin);
+  command[strcspn(command, "\n")] = '\0'; 
+  printf("%s: command not found\n", command);
   return 0;
 }
